3fac171869e5e8ce7f65d24c46a20605d421fea3
Chhavi Paliwal <chhavipaliwal1203@gmail.com>
2025-02-20 22:50:13 +0530

    Refactor Start page layout and styling with improved structure and updated icons

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,14 @@
 import Banner from '@/components/sections/homepage/banner';
 import React from 'react';
-import Image from 'next/image';
-import { Code, navbar } from '@heroui/react';
 import Start from './start/page';
 
 export default function Home() {
   return (
     <>
-      <Start />
+      {/* <Navbar/> */}
       <Banner />
+      {/* <Banner/> */}
+      <Start />
     </>
   );
 }
