6cfcbe299147042c850a280ab54b80a1060a2c1d
Chhavi Paliwal <chhavipaliwal1203@gmail.com>
2025-02-20 22:51:40 +0530

    Remove commented Navbar and Banner components; add git error message file for reference

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
 import Banner from '@/components/sections/homepage/banner';
 import React from 'react';
+import Image from 'next/image';
+import { Code, navbar } from '@heroui/react';
 import Start from './start/page';
 
 export default function Home() {
   return (
     <>
-      {/* <Navbar/> */}
       <Banner />
-      {/* <Banner/> */}
       <Start />
     </>
   );
