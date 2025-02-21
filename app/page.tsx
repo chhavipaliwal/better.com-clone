043794ad9d714ba529917436dbd0323e2920d94b
Chhavi Paliwal <chhavipaliwal1203@gmail.com>
2025-02-22 02:39:41 +0530

    Import Start page component on homepage
    - Add import for Start page component in main page
    - Prepare for integrating start page content into homepage layout

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Banner from '@/components/sections/homepage/banner';
 import Testimonials from '@/components/sections/homepage/testimonials';
 import Qna from '@/components/sections/homepage/qna';
+import Start from './start/page';
 export default function Home() {
   return (
     <>
