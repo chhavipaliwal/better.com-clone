c957977f3ccb60d44d34417fb7ee58478c1520de
Chhavi Paliwal <chhavipaliwal1203@gmail.com>
2025-02-21 23:55:32 +0530

    Refactor mortgage calculator into separate components with improved input styling

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -19,14 +19,19 @@ const Input = forwardRef<HTMLInputElement, InputProps>(
       <div className="relative flex flex-col">
         <div className="relative">
           {startContent && (
-            <div className="absolute left-4 top-1/2 -translate-y-1/2">
+            <div
+              className={cn(
+                'absolute left-4 -translate-y-1/2',
+                label ? 'top-[60%]' : 'top-1/2'
+              )}
+            >
               {startContent}
             </div>
           )}
           <input
             ref={ref}
             className={cn(
-              'w-full px-4 py-5 pb-3 font-bold',
+              'w-full px-4 py-6 pb-3 font-bold',
               'rounded-small border border-foreground-200 transition-all hover:border-accent hover:shadow-[0_0_0_4px_inset] hover:shadow-accent focus:outline-none',
               startContent ? 'pl-8' : '',
               endContent ? 'pr-8' : '',
@@ -37,7 +42,7 @@ const Input = forwardRef<HTMLInputElement, InputProps>(
             {...props}
           />
           {endContent && (
-            <div className="absolute right-4 top-1/2 -translate-y-1/2">
+            <div className="absolute right-4 top-[60%] -translate-y-1/2">
               {endContent}
             </div>
           )}
@@ -47,8 +52,7 @@ const Input = forwardRef<HTMLInputElement, InputProps>(
                 'absolute left-4 transition-all duration-200',
                 isFocused || hasValue || startContent || endContent
                   ? 'top-2 px-1 text-xs'
-                  : 'top-1/2 -translate-y-1/2 text-sm text-foreground-400',
-                isFocused ? 'text-accent' : ''
+                  : 'top-1/2 -translate-y-1/2 text-sm text-foreground-400'
               )}
             >
               {label}
